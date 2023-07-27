a2779bbdefd1660cdbcfe2652e1bca696683e0cf
Yargi <markus.krusch@gmail.com>
2023-07-28 01:41:28 +0200

    generated first components and spread the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReceiptsListComponent } from './receipts-list/receipts-list.component';
+import { NewsListComponent } from './news-list/news-list.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceiptsListComponent,
+    NewsListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
