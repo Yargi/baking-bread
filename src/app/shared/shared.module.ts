b0734f5842398ed56661da386124b26ce7cfe9e0
Yargi <markus.krusch@gmail.com>
2023-08-01 11:57:10 +0200

    loader component added to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersOnlyComponent } from './users-only/users-only.component';
+import { LoadingComponent } from './loading/loading.component';
 
 
 
 @NgModule({
   declarations: [
-    UsersOnlyComponent
+    UsersOnlyComponent,
+    LoadingComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [
-    UsersOnlyComponent
-  ]
+    exports: [
+        UsersOnlyComponent,
+        LoadingComponent
+    ]
 })
 export class SharedModule { }
