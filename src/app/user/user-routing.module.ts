551926f832d0fb926176e6020810e90bc2cb0237
Markus Krusch <markus.krusch@gmail.com>
2023-08-02 21:04:06 +0200

    user routing module added

---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,35 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ProfileComponent} from "./profile/profile.component";
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+    // canActivate: [AuthActivate],
+    // data: {
+    //   showAfterAuth: false
+    // }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    // canActivate: [AuthActivate],
+    // data: {
+    //   showAfterAuth: false
+    // }
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    // canActivate: [AuthActivate],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class UserRoutingModule {}
