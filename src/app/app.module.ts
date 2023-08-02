87d472ea9f3301e2f7e6b32c98c6fa5c15fb4f05
Yargi <markus.krusch@gmail.com>
2023-08-02 23:04:18 +0200

    fixed imports and declarations after moving components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgOptimizedImage} from "@angular/common";
 import {CoreModule} from "./core/core.module";
 import {SharedModule} from "./shared/shared.module";
+import {NewsListComponent} from "./news-list/news-list.component";
+import {RecipesListComponent} from "./recipes-list/recipes-list.component";
+import {CommentsListComponent} from "./comments-list/comments-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    NewsListComponent,
+    RecipesListComponent,
+    CommentsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgOptimizedImage,
     CoreModule,
     SharedModule
   ],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
-import {NewsListComponent} from "../news-list/news-list.component";
-import {RecipesListComponent} from "../recipes-list/recipes-list.component";
 import {SharedModule} from "../shared/shared.module";
 import { NavigationComponent } from './navigation/navigation.component';
 import { CommentsListComponent } from '../comments-list/comments-list.component';
@@ -14,10 +12,7 @@ import { CommentsListComponent } from '../comments-list/comments-list.component'
   declarations: [
     HeaderComponent,
     FooterComponent,
-    NewsListComponent,
-    RecipesListComponent,
     NavigationComponent,
-    CommentsListComponent
   ],
     imports: [
         CommonModule,
@@ -27,8 +22,6 @@ import { CommentsListComponent } from '../comments-list/comments-list.component'
   exports: [
     HeaderComponent,
     FooterComponent,
-    NewsListComponent,
-    RecipesListComponent
   ]
 })
 export class CoreModule { }
